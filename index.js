5207a5c888a5f11e70b454c91d764f658681cb8b
Ayesha <ayesha@gmail.com>
2026-01-10 02:55:41 +0500

    add PORT for deploy

--- index.js
+++ index.js
@@ -23,3 +23,9 @@ mongoose
     app.listen(5000, () => console.log("Server running on 5000"));
   })
   .catch((err) => console.log(err));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
+ 
