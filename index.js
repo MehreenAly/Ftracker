016f4ea8dbcfa89fd9bafe8f7787d19e85ca00d4
Ayesha <ayesha@gmail.com>
2026-01-13 01:04:20 +0500

    fix mongodb connection for vercel

--- index.js
+++ index.js
@@ -18,11 +18,28 @@ app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => console.log(err));
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+async function dbConnect() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+      serverSelectionTimeoutMS: 5000,
+    }).then(m => m);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
+dbConnect().then(() => console.log("MongoDB connected"));
+
+
 
 module.exports = app;
