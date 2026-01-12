affb6f38aba04990907f428b385dfbaac9720fc1
Ayesha <ayesha@gmail.com>
2026-01-13 02:31:42 +0500

    fix mongo connection

--- index.js
+++ index.js
@@ -7,23 +7,26 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-// routes
 const transactionRoutes = require("./routes/transactionroutes");
 app.use("/api/transactions", transactionRoutes);
 
-// test route
 app.get("/", (req, res) => {
   res.send("Backend running");
 });
 
-// ✅ SIMPLE MongoDB connect (WORKS on Vercel)
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB error:", err);
+let isConnected = false;
+
+async function connectDB() {
+  if (isConnected) return;
+
+  await mongoose.connect(process.env.MONGO_URI, {
+    serverSelectionTimeoutMS: 5000,
   });
+
+  isConnected = true;
+  console.log("MongoDB connected");
+}
+
+connectDB();
 
 module.exports = app;
