30a38b2efccc628a72105b6d59bd6fcac688ef01
Ayesha <www.islaheamal@gmail.com>
2026-02-14 23:20:41 +0500

    fix express init

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const app = express;
+const app = express();   // ✅ FIX HERE
 
 app.use(express.json());
 
